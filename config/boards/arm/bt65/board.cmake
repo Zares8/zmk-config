a77f5b4b1aaabad6dd40ac5620a244c81ae9837d
Zares8 <barhanstefan@gmail.com>
2023-10-24 21:14:50 +0200

    GG_WP

--- config/boards/arm/bt65/board.cmake
+++ config/boards/arm/bt65/board.cmake
@@ -1,5 +1,13 @@
+# Copyright (c) 2020 The ZMK Contributors
 # SPDX-License-Identifier: MIT
 
 board_runner_args(nrfjprog "--nrf-family=NRF52" "--softreset")
 include(${ZEPHYR_BASE}/boards/common/blackmagicprobe.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+
+zephyr_include_directories(.)
+
+zephyr_library()
+
+zephyr_library_sources(ec11.c)
+zephyr_library_sources_ifdef(CONFIG_EC11_TRIGGER ec11_trigger.c)
